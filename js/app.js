f9278dc6c4e2eebcda250eba04f63ab0c686cf70
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-23 07:12:07 -0500

    arreglando version 2

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ window.addEventListener("load", function() {
 		document.getElementById("tareas").classList.remove("ocultar");
 		document.getElementById("btn").classList.add("ocultar");
 	}
+	
 	var guardarTarea = document.getElementById("btnGuardar");
 	guardarTarea.addEventListener("click", function(e) {
 		e.preventDefault();
@@ -22,11 +23,42 @@ window.addEventListener("load", function() {
 		titulo.innerHTML = tarea;
 		titulo.classList.add("titulo");
 		tareas.appendChild(titulo);
-		var agregarNewTarea= document.createElement("a");
-		agregarNewTarea.innerHTML = "Agrega una nueva tarea";
-		agregarNewTarea.setAttribute("href","#");
-		agregarNewTarea.classList.add("agregarNuevaTarea");
+		var agregarNewTarea= document.createElement("div");
+		agregarNewTarea.innerHTML = "Agrega una nueva tarea";	agregarNewTarea.classList.add("agregarNuevaTarea");
 		tareas.appendChild(agregarNewTarea);
 		document.getElementById("form").classList.add("ocultar");
     }
-});
+	
+	var agregarNuevaTarea = document.getElementsByClassName("agregarNuevaTarea");
+	agregarNuevaTarea.addEventListener("click", function(e) {
+		e.preventDefault();
+		agregarTareaLista();
+	});
+	function agregarTareaLista(){
+		/*var nuevoTextArea = document.createElement(textarea);
+    	var textnode = document.createTextNode(" ");
+    	nuevoTextArea.appendChild(textnode);
+    	var list = document.getElementsByClassName("tareas");
+    	list.insertBefore(nuevoTextArea, list.childNodes[0]);
+		
+		var save=document.createElement("button");
+		save.classList.add("");*/
+		var newItem = document.createElement("LI");
+		var textnode = document.createTextNode("Water");
+		newItem.appendChild(textnode);
+		var list = document.getElementsByClass("titulo");
+		list.insertBefore(newItem, list.childNodes[0]);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
