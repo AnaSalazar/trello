ad879e771440e13e00a59ef6d6f3f6efd9fa4862
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-27 15:04:04 -0500

    subiendo versión 3

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", function() {
 	var btn = document.getElementById("btn");
 	var tarea = document.getElementById("inputPrimero").value;
+	var tareas = document.getElementById("tareas");
+	var formulario = document.getElementById("form");
+	var creado = false;
 	btn.addEventListener("click", function(e) {
 		e.preventDefault();
 		agregarTarea();
@@ -17,55 +20,53 @@ window.addEventListener("load", function() {
 	});
 	function crearTarea(){
 		var tarea = document.getElementById("inputPrimero").value;
-		var tareas = document.getElementById("tareas");
-    	var titulo = document.createElement("div");
-		var textarea = document.createElement("textarea");
-		var boton = document.createElement("div");
+		var titulo = document.createElement("div");
 		titulo.innerHTML = tarea;
 		titulo.classList.add("titulo");
 		tareas.appendChild(titulo);
 		var agregarNewTarea= document.createElement("div");
-		agregarNewTarea.innerHTML = "Agrega una nueva tarea";	agregarNewTarea.classList.add("agregarNuevaTarea");
+		agregarNewTarea.innerHTML = "Agrega una nueva tarjeta";	agregarNewTarea.classList.add("agregarNuevaTarea");
 		tareas.appendChild(agregarNewTarea);
-		document.getElementById("form").classList.add("ocultar");
-		var agg = document.querySelector(".agregarNuevaTarea");
-		agg.addEventListener("click", function(e) {
-			anadirText();
-			document.getElementById("tareas").lastElementChild.addEventListener("click", function() {
-				guardarActividad();
-			});
-			function guardarActividad(){
-				var texto=textarea.value;
-				var tareaText = document.createElement("div");
-				tareaText.innerHTML=texto;
-				tareas.appendChild(tareaText);
-				tareaText.classList.add("tareaNueva");
-				textarea.style.display="none";
-				boton.style.display="none";
-				var agregarNewTarea= document.createElement("div");
-				agregarNewTarea.innerHTML = "Agrega una nueva tarea";	agregarNewTarea.classList.add("agregarNuevaTarea");
-				tareas.appendChild(agregarNewTarea);
-			};
+		formulario.classList.add("ocultar");
+
+		agregarNewTarea.addEventListener("click", function() {
+			anadirText(this);	
 		});
-		function anadirText(){
-			agg.style.display = "none";
-			tareas.appendChild(textarea);
+    }
+	function guardarActividad(boton){
+		var textarea = boton.previousSibling;
+		var agregarNewTarea = textarea.previousSibling;
+		var texto=textarea.value;
+		var tareaText = document.createElement("div");
+		tareaText.classList.add("tareaNueva");
+		tareaText.innerHTML=texto;
+		tareas.insertBefore(tareaText,agregarNewTarea);
+		textarea.style.display="none";
+		boton.style.display="none";
+		agregarNewTarea.style.display="block";
+	};
+	function anadirText(agregarNewTarea){
+		var boton, textarea;
+		if (!creado) {
+			textarea = document.createElement("textarea");
 			textarea.classList.add("textarea");
+			boton = document.createElement("div");
+			tareas.appendChild(textarea);
 			tareas.appendChild(boton);
 			boton.classList.add("btnAnadir");
 			boton.innerHTML="Añadir";
+			boton.addEventListener("click", function() {
+			guardarActividad(this);
+			});
+			creado = true;
+		} else {
+			textarea = document.querySelector(".textarea");
+			boton = document.querySelector(".btnAnadir");
 		}
-    }
+		agregarNewTarea.style.display = "none";
+		textarea.value="";
+		textarea.style.display="block";
+		boton.style.display="inline-block";
+		
+	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
