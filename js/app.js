638752f27ddebd5076a2d7809738de685dfce636
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-10-06 09:32:18 -0500

    añadiendo que se puedan añadir varias tarjetas

--- js/app.js
+++ js/app.js
@@ -1,75 +1,68 @@
 window.addEventListener("load", function() {
+	var contenedor = document.getElementById("contenedor");
 	var btn = document.getElementById("btn");
-	var tarea = document.getElementById("inputPrimero").value;
+	var tarea = document.getElementById("inputPrimero");
+	var formulario = document.getElementById("contenedor-formulario");
 	var tareas = document.getElementById("tareas");
-	var formulario = document.getElementById("form");
-	var creado = false;
-	tareas.style.display="none";
+	var form = document.getElementById("form");
+	form.style.display="none";
 	btn.addEventListener("click", function(e) {
 		e.preventDefault();
 		agregarTarea();
-		
+		tarea.focus();
 	});
 	function agregarTarea() {
-		document.getElementById("btn").style.display="none";
-		tareas.style.display="inline-block";
-		
+		btn.style.display="none";
+		form.style.display="inline-block";
 	}
 	var guardarTarea = document.getElementById("btnGuardar");
 	guardarTarea.addEventListener("click", function(e) {
 		e.preventDefault();
 		crearTarea();
 		btn.style.display="inline-block";
+		tarea.value= "";
 	});
 	function crearTarea(){
+
+		form.style.display="none";
+
 		var tarea = document.getElementById("inputPrimero").value;
+		var tareas = document.createElement("div");
+		tareas.classList.add("verde");
 		var titulo = document.createElement("div");
 		titulo.innerHTML = tarea;
 		titulo.classList.add("titulo");
+		contenedor.insertBefore(tareas, contenedor.lastElementChild);
 		tareas.appendChild(titulo);
 		var agregarNewTarea= document.createElement("div");
 		agregarNewTarea.innerHTML = "Agrega una nueva tarjeta";	agregarNewTarea.classList.add("agregarNuevaTarea");
 		tareas.appendChild(agregarNewTarea);
-		formulario.style.display="none";
-
-		agregarNewTarea.addEventListener("click", function() {
-			anadirText(this);	
-		});
-    }
-	function guardarActividad(boton){
-		var textarea = boton.previousSibling;
-		var agregarNewTarea = textarea.previousSibling;
-		var texto=textarea.value;
-		var tareaText = document.createElement("div");
-		tareaText.classList.add("tareaNueva");
-		tareaText.innerHTML=texto;
-		tareas.insertBefore(tareaText,agregarNewTarea);
-		textarea.style.display="none";
-		boton.style.display="none";
-		agregarNewTarea.style.display="block";
-	};
-	function anadirText(agregarNewTarea){
-		var boton, textarea;
-		if (!creado) {
-			textarea = document.createElement("textarea");
-			textarea.classList.add("textarea");
-			boton = document.createElement("div");
-			tareas.appendChild(textarea);
-			tareas.appendChild(boton);
-			boton.classList.add("btnAnadir");
-			boton.innerHTML="Añadir";
-			boton.addEventListener("click", function() {
-			guardarActividad(this);
+		var array = document.querySelectorAll(".agregarNuevaTarea");
+		for(var i= 0; i < array.length; i++){
+			array[i].addEventListener("click", function() {
+				this.style.display="none";
+				crearFormulario(tareas,this);
 			});
-			creado = true;
-		} else {
-			textarea = document.querySelector(".textarea");
-			boton = document.querySelector(".btnAnadir");
 		}
-		agregarNewTarea.style.display = "none";
-		textarea.value="";
-		textarea.style.display="block";
-		boton.style.display="inline-block";
-		
+	}
+	function crearFormulario(tareas,a){
+		var form = document.createElement("form");
+		tareas.appendChild(form);
+		var textArea= document.createElement("textarea");
+		textArea.classList.add("textarea");
+		form.appendChild(textArea);
+		var botonGuardarTarea = document.createElement("button");
+		botonGuardarTarea.classList.add("btnAnadir");
+		botonGuardarTarea.innerHTML= "Guardar";
+		form.appendChild(botonGuardarTarea);
+		form.lastElementChild.addEventListener("click", function(e){
+			e.preventDefault();
+			var contenidoText = document.createElement("div");
+			contenidoText.classList.add("tareaNueva");
+			contenidoText.innerHTML= textArea.value;
+			tareas.insertBefore(contenidoText, a);
+			textArea.value="";
+			form.style.display="none";
+		});
 	}
 });
